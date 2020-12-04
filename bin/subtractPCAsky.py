3feb1c203fc7b285ed2f531ba55f8cf617c27e75
Bernd Husemann <husemann@mpia.de>
2020-12-04 16:25:08 +0100

    corrected typo

--- bin/subtractPCAsky.py
+++ bin/subtractPCAsky.py
@@ -30,7 +30,7 @@ def main():
         print('Creating PCA spectral libary. This may take a while... ')
     PCA_out,sky = cube.create_PCA_sky(mask, cont_filt=args.filter_width, spectra=args.spectra)
     hdu = pyfits.PrimaryHDU(PCA_out)
-    hdu.writeto(args.PCA_out, overwrite=True)
+    hdu.writeto('test.fits', overwrite=True)
 
     if args.verbose:
         print('Start subtracting sky line residuals.')
