624426275f418738ad2ea88094daca6f5c5ad27f
Bernd Husemann <husemann@mpia.de>
2020-12-09 11:10:07 +0100

    corrected pbar update

--- cubePCA/cubePCA.py
+++ cubePCA/cubePCA.py
@@ -163,12 +163,12 @@ class IFUCube:
             out = pool.apply_async(remove_PCAsky,args=(spec,pca_specs,cont_filt,select_wave,i,count))
             results.append(out)
 
-        if pbar is not None:
-            while count.value < self.getSpax():
-                pbar.reset(count.value)
-
         for i in range(len(results)):
             (cube,i) = results[i].get()
             self.__hdu[self.extension].data[:,sub_indices[i],:] = cube
+
+        if pbar is not None:
+            while count.value < self.getSpax():
+                pbar.update(count.value - pbar.n)
         pool.close()
         pool.join()
