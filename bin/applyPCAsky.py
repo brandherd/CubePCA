738ba3afd46d2b2bc6dcb5d7d6cca3865bc4e942
Bernd Husemann <husemann@mpia.de>
2020-12-09 11:51:50 +0100

    reduced number of time count.value is accessed

--- bin/applyPCAsky.py
+++ bin/applyPCAsky.py
@@ -13,7 +13,7 @@ def main():
     parser.add_argument('PCA_in', metavar='PCA_in', type=str, help='Output FITS file with PCA component spectra')
     parser.add_argument('-m', '--wave_mask', type=str, default='',help='File name of the wavelength selection for the PCA subtraction. If not given a default wavelength set is being used. An example is given in the wave.txt file.')
     parser.add_argument('-e', '--extension', type=int, default=1, help='extension in which the data should be taken from (Default: 1')
-    parser.add_argument('-c', '--components', type=int, default=20, help='Number of PCA components to be used (Default: 20)')
+    parser.add_argument('-c', '--components', type=int, default=40, help='Number of PCA components to be used (Default: 40)')
     parser.add_argument('-f', '--filter_width', type=int, default=50, help='Size of median filter in wavelength direction to remove continuum signal before sky residual subtraction (Default: 50)')
     parser.add_argument('-p', '--processes', type=str, default='auto', help='number of processes to be used for multiprocessing, auto for maximum otherwise put number (Default: auto)')
     parser.add_argument('--verbose', action='store_true', help='Set if infos are printed to the command line')
