78b8605f32b36bf024da005b2f8ed25f4b08261c
Bernd Husemann <husemann@mpia.de>
2021-12-08 16:03:55 +0100

    updated setup.py to add installation of createMask.py

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setuptools.setup(
     version="0.1",
     author="Bernd Husemann",
     author_email="husemann@mpia.de",
-    description="A small example package",
-    url="https://github.com/pypa/sampleproject",
+    description="A small package for PCA sky subtraction in IFU datacubes",
+    url="https://github.com/brandherd/CubePCA",
     packages = setuptools.find_packages(),
-    scripts=['bin/subtractPCAsky.py','bin/createPCAsky.py','bin/applyPCAsky.py'],
+    scripts=['bin/subtractPCAsky.py','bin/createPCAsky.py','bin/applyPCAsky.py','bin/createMask.py'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
